68af1f75b98a98e00e1a39aad7ce7f4ec4476f92
Rashmini132 <64332289+Rashmini132@users.noreply.github.com>
2020-04-29 22:30:21 -0400

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,14 @@
+
+
+
+//Even Number program without using Modulus % operator
+
+class SimpleIf{
+public static void main(String args[])
+{
+int n=5;
+if((n/2)*2==n)
+{
+System.out.println("Even Number ");
+}}
+}
