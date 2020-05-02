12c380eb968a0020cc48ff8c5a05ecea2420cde1
Rashmini132 <64332289+Rashmini132@users.noreply.github.com>
2020-05-02 11:19:20 -0400

    Add files via upload

---
+++ HwPattern.java
@@ -0,0 +1,8 @@
+class HwPattern
+{ public static void main(String ar[]){
+for(int i=1;i<=5;i++)
+{ for(int j=i;j<=5;j++)
+{ System.out.print(j);
+}
+System.out.println();
+}}}
---
+++ pattern2.java
@@ -0,0 +1,8 @@
+class pattern2
+{ public static void main(String ar[]){ int k=1;
+for(int i=1;i<=5;i++)
+{ for(int j=5;j>=i;j--)
+{ System.out.print(j);
+  }
+System.out.println();}
+}}
