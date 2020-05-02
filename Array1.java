6766b73bc6ddde97ff122748ab09333b60742d6c
Rashmini132 <64332289+Rashmini132@users.noreply.github.com>
2020-05-02 12:55:11 -0400

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,9 @@
+class Array1
+{public static void main(String ar[])
+{ int a[] = {3,4,5,6};
+  for(int i=0;i<a.length;i++) 
+ System.out.print(a[i]+" ");
+System.out.println();
+for(int k:a)
+System.out.print(k +" ");
+}}
