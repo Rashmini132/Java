d5e75edf710ce8a022b129568c5807221e1557a3
Rashmini132 <64332289+Rashmini132@users.noreply.github.com>
2020-05-08 20:02:08 -0400

    Add files via upload

---
+++ Local_InnerClass.java
@@ -0,0 +1,17 @@
+class School
+{ void show()
+ { 
+ class Social
+        {  void display(){
+System.out.println(" marks");}}
+Social y= new Social(); // objects for class social are created inside the method show(), similar to local variable
+ y.display();
+}}
+
+class Local_InnerClass{      // it is a class which is inside method
+public static void main(String ar[]){
+
+ School x = new School();
+ x.show();
+}}
+ 
